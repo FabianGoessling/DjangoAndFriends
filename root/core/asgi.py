69317c237a42e21f80aebdf7b5f57e0df56338f9
FabianGoessling <Fabian.Goessling@uni-muenster.de>
2021-05-23 12:55:33 +0200

    adds init files

--- root/core/asgi.py
+++ root/core/asgi.py
@@ -16,26 +16,26 @@ from django.core.asgi import get_asgi_application
 django_asgi = get_asgi_application()
 
 from channels.routing import get_default_application
+
 from django.urls import path, re_path
-#from api.schema import MyGraphqlWsConsumer
+from api.schema import MyGraphqlWsConsumer
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.consumers import ChatConsumer
-from django.apps import apps
 
-channels_asgi = get_default_application()
+
 
 application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
         URLRouter(
             # bokeh_app_config.routes.get_websocket_urlpatterns()
-            #[path("api/graphql/", MyGraphqlWsConsumer.as_asgi()),
-             [re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi())
+            [path("api/graphql/", MyGraphqlWsConsumer.as_asgi()),
+             re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi())
              ]
         ),
     ),
     'http': AuthMiddlewareStack(URLRouter(
-        [re_path(r"", channels_asgi)]))  # bokeh_app_config.routes.get_http_urlpatterns())),
+        [re_path(r"", django_asgi)]))  # bokeh_app_config.routes.get_http_urlpatterns())),
 })
 
 # import os
